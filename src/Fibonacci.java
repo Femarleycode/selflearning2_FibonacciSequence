caca1229713304d85dddf62adf21eff036946a6a
femarleycode <femarleycode@gmail.com>
2020-04-09 12:28:27 +0100

    second commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,18 @@
+
+public class Fibonacci {
+
+	public void runFib(int limit) {
+		int num1 = 0;
+		int num2 = 1;
+
+		System.out.println(num1);
+		System.out.println(num2);
+		for (int sum = 0; sum < limit;) {
+			sum = num1 + num2;
+			num1 = num2;
+			num2 = sum;
+			System.out.println(sum);
+		}
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Fibonacci runIt = new Fibonacci();
+		runIt.runFib(1_000_000);
+	}
+
+}
